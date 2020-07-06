c46cd3efcb38f6215f96296dd437393cb3f4dc83
jin10086 <igaojin@qq.com>
2020-07-06 18:49:54 +0800

    Update app.py

--- src/app.py
+++ src/app.py
@@ -157,7 +157,7 @@ def get_comp_order_book():
                 before_data = result[name]
                 if msg != before_data:
                     sendMail(
-                        "COMP有挂单超过50了",
+                        f"{k} 有挂单超过50了",
                         send_txt,
                         ["igaojin@qq.com", "woody168@gmail.com"],
                     )
